de726eac3f1dc95b81cc928579e64818621c628f
Dario Alexander Diaz Florez <ingdariodiaz@gmail.com>
2021-07-12 21:30:03 -0500

    Curso Angular. Fin sección 13

--- 05-heroesApp/src/app/app.module.ts
+++ 05-heroesApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -14,7 +15,8 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
