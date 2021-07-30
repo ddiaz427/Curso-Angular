4033793b3541db9d37fb4c9b420012bdaece1240
Dario Alexander Diaz Florez <ingdariodiaz@gmail.com>
2021-07-30 07:19:37 -0500

    Curso Angular. Fin Sección 17: Formularios Reactivos

--- 06-formulariosApp/src/app/reactive/basicos/basicos.component.ts
+++ 06-formulariosApp/src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -6,11 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent {
 
-  constructor() { }
+  // miFormulario: FormGroup = new FormGroup({
+  //   nombre: new FormControl('RTX 4080TI'),
+  //   precio: new FormControl(150000),
+  //   existencias: new FormControl(16),
+  // })
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [ , [Validators.required, Validators.minLength(3)]],
+    precio: [ , [Validators.required, Validators.min(1)]],
+    existencias: [ , [Validators.required, Validators.min(1)]]
+  });
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) { }
+
+
+  campoNoValido(campo: string){
+    return this.miFormulario.controls[campo].errors && this.miFormulario.controls[campo].touched;
+  }
+
+  guardar(){
+    if(this.miFormulario.invalid){
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset();
+
   }
 
 }
